- Inference works now much better, fixed an issue with input scaling

components/inference/model_components.py
@@ -2,11 +2,12 @@ import torch
 import torch.nn as nn
 from glob import glob
 
-from BoneEnhance.components.models import EnhanceNet, ConvNet, PerceptualNet
+from BoneEnhance.components.models import EnhanceNet, ConvNet, PerceptualNet, SREncoderDecoder
+from collagen.modelzoo.segmentation import EncoderDecoder
 
 
 class InferenceModel(nn.Module):
-    def __init__(self, models_list):
+    def __init__(self, models_list, sigmoid=False):
         super(InferenceModel, self).__init__()
         self.n_folds = len(models_list)
         modules = {}
@@ -14,6 +15,7 @@ class InferenceModel(nn.Module):
             modules[f'fold_{idx}'] = m
 
         self.__dict__['_modules'] = modules
+        self.sigmoid = sigmoid
 
     def forward(self, x):
         res = 0
@@ -21,6 +23,8 @@ class InferenceModel(nn.Module):
         for idx in range(self.n_folds):
             fold = self.__dict__['_modules'][f'fold_{idx}']
             pred = fold(x)
+            if self.sigmoid:
+                pred = pred.sigmoid()
             res += pred
             preds.append(pred)
 
@@ -35,35 +39,39 @@ def load_models(model_path, config, n_gpus=1, magnification=4, fold=None):
         models = glob(model_path + '/*fold_*.pth')
     models.sort()
     vol = len(config.training.crop_small) == 3
-
-    available_models = {
-        'enhance': EnhanceNet(config.training.crop_small, config.training.magnification,
-                              activation=config.training.activation,
-                              add_residual=config.training.add_residual,
-                              upscale_input=config.training.upscale_input),
-        'convnet': ConvNet(config.training.magnification,
-                           activation=config.training.activation,
-                           upscale_input=config.training.upscale_input,
-                           n_blocks=config.training.n_blocks,
-                           normalization=config.training.normalization),
-        'perceptualnet': PerceptualNet(config.training.magnification,
-                                       resize_convolution=config.training.upscale_input,
-                                       norm=config.training.normalization,
-                                       vol=vol, rgb=config.training.rgb),
-    }
+    architecture = config.training.architecture
 
     # List the models
     model_list = []
     for fold in range(len(models)):
-        if n_gpus > 1:
-            #model = nn.DataParallel(
-            #    available_models[config.training.architecture])
-            model = nn.DataParallel(PerceptualNet(config.training.magnification,
-                                                  resize_convolution=config.training.upscale_input,
-                                                  norm=config.training.normalization,
-                                                  vol=vol, rgb=config.training.rgb))
+
+        if architecture == 'srencoderdecoder':
+            config.model.magnification = config.training.magnification
+            model = SREncoderDecoder(**config['model'])
+        elif architecture == 'encoderdecoder':
+            model = EncoderDecoder(**config['model'])
+        elif architecture == 'enhance':
+            model = EnhanceNet(config.training.crop_small, config.training.magnification,
+                               activation=config.training.activation,
+                               add_residual=config.training.add_residual,
+                               upscale_input=config.training.upscale_input)
+        elif architecture == 'convnet':
+            model = ConvNet(config.training.magnification,
+                            activation=config.training.activation,
+                            upscale_input=config.training.upscale_input,
+                            n_blocks=config.training.n_blocks,
+                            normalization=config.training.normalization)
+        elif architecture == 'perceptualnet':
+            model = PerceptualNet(config.training.magnification,
+                                  resize_convolution=config.training.upscale_input,
+                                  norm=config.training.normalization,
+                                  vol=vol, rgb=config.training.rgb)
         else:
-            model = available_models[config.training.architecture]
+            raise Exception('Model architecture unavailable.')
+
+        if n_gpus > 1:
+            model = nn.DataParallel(model)
+
         model.load_state_dict(torch.load(models[fold]))
         model_list.append(model)
 

components/inference/pipeline_components.py
@@ -27,22 +27,29 @@ def inference(inference_model, args, config, img_full, device='cuda', weight='me
     Calculates inference on one image.
     """
 
-    mag = config.training.magnification
-    input_x = config['training']['crop_small'][0]
-    input_y = config['training']['crop_small'][1]
+    # Input variables
+    input_x = config.training.crop_small[0]
+    input_y = config.training.crop_small[1]
     x, y, ch = img_full.shape
-    x_out, y_out = x * mag, y * mag
+
+    # Segmentation model does not upscale the image
+    if config.training.architecture == 'encoderdecoder':
+        mag = 1
+        x_out, y_out = x, y
+        input_x *= config.training.magnification
+        input_y *= config.training.magnification
+    else:
+        mag = config.training.magnification
+        x_out, y_out = x * mag, y * mag
 
     # Cut large image into overlapping tiles
     tiler = ImageSlicer(img_full.shape, tile_size=(input_x, input_y),
                         tile_step=(input_x // tile, input_y // tile), weight=weight)
-                        #tile_step=(input_x, input_y), weight=weight)
 
     x_tile = np.min((input_x * mag, x_out))
     y_tile = np.min((input_y * mag, y_out))
     tiler_out = ImageSlicer((x_out, y_out, ch), tile_size=(x_tile, y_tile),
                             tile_step=(x_tile // tile, y_tile // tile), weight=weight)
-                            #tile_step=(x_tile, y_tile), weight=weight)
 
     # HCW -> CHW. Optionally, do normalization here
     tiles = [tensor_from_rgb_image(tile) for tile in tiler.split(img_full)]
@@ -68,9 +75,9 @@ def inference(inference_model, args, config, img_full, device='cuda', weight='me
         if plot:
             for i in range(args.bs):
                 if args.bs != 1 and pred_batch.shape[0] != 1:
-                    plt.imshow(pred_batch.cpu().detach().numpy().astype('float32').transpose(0, 2, 3, 1)[i, :, :])
+                    plt.imshow(pred_batch.cpu().detach().numpy().astype('float32').transpose(0, 2, 3, 1)[i, :, :].squeeze())
                 else:
-                    plt.imshow(pred_batch.cpu().detach().numpy().astype('float32').squeeze().transpose(1, 2, 0))
+                    plt.imshow(pred_batch.cpu().detach().numpy().astype('float32').squeeze().transpose(1, 2, 0).squeeze())
                 plt.show()
 
         # Check for inconsistencies
@@ -133,8 +140,8 @@ def inference_3d(inference_model, args, config, img_full, device='cuda', weight=
         # Move tile to GPU
         if mean is not None and std is not None:
             tiles_batch = tiles_batch.float()
-            for ch in range(len(mean)):
-                tiles_batch[:, ch, :, :] = ((tiles_batch[:, ch, :, :] - mean[ch]) / std[ch])
+            for c in range(len(mean)):
+                tiles_batch[:, c, :, :] = (((tiles_batch[:, c, :, :] / 255.) - mean[c]) / std[c])
             tiles_batch = tiles_batch.to(device)
         else:
             tiles_batch = (tiles_batch.float() / 255.).to(device)

components/inference/tiler3d.py
@@ -92,7 +92,7 @@ class Tiler3D:
         target_shape = self.target_shape
 
         image = np.zeros((channels, target_shape[0], target_shape[1], target_shape[2]), dtype=dtype)
-        norm_mask = np.zeros((1, target_shape[0], target_shape[1], target_shape[2]), dtype=dtype    )
+        norm_mask = np.zeros((1, target_shape[0], target_shape[1], target_shape[2]), dtype=dtype)
 
         #w = np.dstack([self.weight] * channels)
         w = np.expand_dims(self.weight, axis=0)

scripts/inference_tiles_2d.py
@@ -12,6 +12,7 @@ from time import time
 from tqdm import tqdm
 from glob import glob
 from scipy.ndimage import zoom
+from skimage.transform import resize
 from omegaconf import OmegaConf
 
 from BoneEnhance.components.utilities import load, save, print_orthogonal, render_volume
@@ -29,11 +30,15 @@ if __name__ == "__main__":
     #snap = 'dios-erc-gpu_2020_10_19_14_09_24_3D_perceptualnet'
     #snap = 'dios-erc-gpu_2020_09_30_14_14_42_perceptualnet_noscaling_3x3_cm_curated_trainloss'
     snap = '2021_01_08_09_49_45_2D_perceptualnet_ds_16'  # 2D model, 3 working folds
+    snap = '2021_02_04_13_02_05_rn18_fpn'  # Segmentation model
+    snap = '2021_02_10_05_29_09_rn50_UNet'
+    snap = '2021_02_10_05_29_09_rn50_fpn'
+    #snap = '2021_02_04_13_02_05_rn34_fpn'  # Gives error
 
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset_root', type=Path, default='/media/dios/kaappi/Santeri/BoneEnhance/Clinical data')
-    #parser.add_argument('--dataset_root', type=Path, default='../../Data/Test set (KP02)/input_original')
+    #parser.add_argument('--dataset_root', type=Path, default='/media/dios/kaappi/Santeri/BoneEnhance/Clinical data')
+    parser.add_argument('--dataset_root', type=Path, default='../../Data/Test set (KP02)/input_original')
     parser.add_argument('--save_dir', type=Path, default=f'../../Data/predictions_2D/{snap}')
     parser.add_argument('--subdir', type=Path, choices=['NN_prediction', ''], default='')
     parser.add_argument('--bs', type=int, default=12)
@@ -60,6 +65,8 @@ if __name__ == "__main__":
     args.save_dir.mkdir(exist_ok=True)
     (args.save_dir / 'visualizations').mkdir(exist_ok=True)
 
+    segmentation = config.training.architecture == 'encoderdecoder'
+
     # Load models
     models = glob(str(args.snapshot) + '/*fold_[0-9]_*.pth')
     #models = glob(str(args.snapshot) + '/*fold_3_*.pth')
@@ -75,9 +82,9 @@ if __name__ == "__main__":
     mean, std = tmp['mean'], tmp['std']
 
     # List the models
-    model_list = load_models(str(args.snapshot), config, n_gpus=args_experiment.gpus, fold=0)
+    model_list = load_models(str(args.snapshot), config, n_gpus=args_experiment.gpus, fold=None)
 
-    model = InferenceModel(model_list).to(device)
+    model = InferenceModel(model_list, sigmoid=segmentation).to(device)
     model.eval()
     print(f'Found {len(model_list)} models.')
 
@@ -86,7 +93,7 @@ if __name__ == "__main__":
     samples = os.listdir(args.dataset_root)
     samples.sort()
 
-    samples = [samples[id] for id in [6]]  # Get intended samples from list
+    #samples = [samples[id] for id in [6]]  # Get intended samples from list
 
     # Skip the completed samples
     if args.completed > 0:
@@ -100,19 +107,28 @@ if __name__ == "__main__":
         data_xy, files = load(str(args.dataset_root / sample), rgb=config.training.rgb, axis=(1, 2, 0))
         x, y, z, ch = data_xy.shape
 
+        # Interpolate image size for segmentation
+        if segmentation:
+            new_size = (data_xy.shape[0] * mag, data_xy.shape[1] * mag, data_xy.shape[2] * mag, 3)
+            data_xy = resize(data_xy, new_size, order=3, preserve_range=True) / 255.
+
+            # Output shape
+            out_xy = np.zeros((x * mag, y * mag, z * mag))
+        else:
+            # Output shape
+            out_xy = np.zeros((x * mag, y * mag, z))
+
         print_orthogonal(data_xy[:, :, :, 0], invert=True, res=0.2, title='Input', cbar=True,
                          savepath=str(args.save_dir / 'visualizations' / (sample + '_input.png')),
                          scale_factor=1000)
-
-        # Output shape
-        out_xy = np.zeros((x * mag, y * mag, z))
 
         # Loop for image slices
         # 1st orientation
         with torch.no_grad():  # Do not update gradients
 
             for slice_idx in tqdm(range(data_xy.shape[2]), desc='Running inference, XY'):
-                out_xy[:, :, slice_idx] = inference(model, args, config, data_xy[:, :, slice_idx, :], tile=args.tile)
+                out_xy[:, :, slice_idx] = inference(model, args, config, data_xy[:, :, slice_idx, :], tile=args.tile,
+                                                    mean=mean, std=std)
 
         # Scale the dynamic range
         out_xy -= np.min(out_xy)

scripts/inference_tiles_3d.py
@@ -29,10 +29,11 @@ if __name__ == "__main__":
 
     # snap = 'dios-erc-gpu_2020_10_12_09_40_33_perceptualnet_newsplit'
     #snap = 'dios-erc-gpu_2020_11_04_15_58_20_3D_perceptualnet_ds'
-    snap = 'dios-erc-gpu_2020_11_04_14_10_25_3D_perceptualnet_scratch'  # Perceptual scratch
+    snap = 'dios-erc-gpu_2020_11_04_14_10_25_3D_perceptualnet_scratch'  # Perceptual scratch, abstract
     # snap = 'dios-erc-gpu_2020_09_30_14_14_42_perceptualnet_noscaling_3x3_cm_curated_trainloss'
-    snap = '2020_12_11_07_10_16_3D_perceptualnet_ds_16'  # Intensity augmentations applied
-    ds = False
+    #snap = '2020_12_11_07_10_16_3D_perceptualnet_ds_16'  # Intensity augmentations applied
+    #snap = '2021_01_11_05_41_47_3D_perceptualnet_ds_autoencoder_16'  # Autoencoder, 4 folds, 2 layers
+    ds = True
 
     parser = argparse.ArgumentParser()
     if ds:
@@ -117,7 +118,7 @@ if __name__ == "__main__":
         # Loop for image slices
         # 1st orientation
         with torch.no_grad():  # Do not update gradients
-            out_xy = inference_3d(model, args, config, data_xy, step=args.step)
+            out_xy = inference_3d(model, args, config, data_xy, step=args.step, mean=mean, std=std)
 
         # Average probability maps
         mask_avg = out_xy

scripts/inference_tiles_large_3d.py
@@ -46,11 +46,12 @@ if __name__ == "__main__":
     parser.add_argument('--plot', type=bool, default=False)
     parser.add_argument('--weight', type=str, choices=['pyramid', 'mean'], default='mean')
     parser.add_argument('--completed', type=int, default=0)
-    parser.add_argument('--step', type=int, default=3, help='Factor for tile step size. 1=no overlap, 2=50% overlap...')
+    parser.add_argument('--step', type=int, default=1, help='Factor for tile step size. 1=no overlap, 2=50% overlap...')
     parser.add_argument('--avg_planes', type=bool, default=False)
     parser.add_argument('--cuda', type=bool, default=False, help='Whether to merge the inference tiles on GPU or CPU')
     parser.add_argument('--mask', type=bool, default=False, help='Whether to remove background with postprocessing')
     parser.add_argument('--scale', type=bool, default=True, help='Whether to scale prediction to full dynamic range')
+    parser.add_argument('--calculate_mean_std', type=bool, default=True, help='Whether to calculate individual mean and std')
     parser.add_argument('--snapshot', type=Path, default=f'../../Workdir/snapshots/{snap}')
     parser.add_argument('--dtype', type=str, choices=['.bmp', '.png', '.tif'], default='.bmp')
     args = parser.parse_args()
@@ -93,7 +94,7 @@ if __name__ == "__main__":
     # samples = [os.path.basename(x) for x in glob(str(args.dataset_root / '*XZ'))]  # Load with specific name
     samples = os.listdir(args.dataset_root)
     samples.sort()
-    samples = [samples[id] for id in [6]]  # Get intended samples from list
+    samples = [samples[id] for id in [2]]  # Get intended samples from list
 
     # Skip the completed samples
     if args.completed > 0:
@@ -120,10 +121,15 @@ if __name__ == "__main__":
         print_orthogonal(data_xy[:, :, :, 0], invert=True, res=0.2, title='Input', cbar=True,
                          savepath=str(args.save_dir / 'visualizations' / (sample[:-3] + '_input.png')), scale_factor=10)
 
+        # Calculate mean and std from the sample
+        if args.calculate_mean_std:
+            mean = [np.mean(data_xy) / 255]
+            std = [np.std(data_xy) / 255]
+
         # Loop for image slices
         # 1st orientation
         with torch.no_grad():  # Do not update gradients
-            prediction = inference_3d(model, args, config, data_xy, step=args.step, cuda=args.cuda)
+            prediction = inference_3d(model, args, config, data_xy, step=args.step, cuda=args.cuda, mean=mean, std=std)
             #prediction, _ = load(str(args.save_dir / sample[:-3]), axis=(1, 2, 0))
             #print_orthogonal(prediction, invert=True, res=50 / 1000, title='Output', cbar=True,
             #                 savepath=str(args.save_dir / 'visualizations' / (sample[:-3] + '_prediction.png')),