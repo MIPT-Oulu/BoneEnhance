- 2D inference is now working correct. Huge improvement on 2D perceptual loss model - Plane averaging is still misaligned

bone_enhance/inference/pipeline_components.py
@@ -28,7 +28,7 @@ from deeppipeline.segmentation.evaluation.metrics import calculate_iou, calculat
 
 
 def inference(inference_model, args, config, img_full, device='cuda', weight='mean', plot=False, mean=None, std=None,
-              tile=2):
+              step=2, cuda=True):
     """
     Calculates inference on one image.
     """
@@ -38,43 +38,49 @@ def inference(inference_model, args, config, img_full, device='cuda', weight='me
         print('No mean or std given! Data will not be scaled prior inference!')
 
     # Input variables
-    input_x = config.training.crop_small[0]
-    input_y = config.training.crop_small[1]
+    tile = list(config.training.crop_small)
     x, y, ch = img_full.shape
+
+    # Scale mean and std to appropriate range (float instead of uint8)
+    if mean.mean() > 1 or std.mean() > 1:
+        mean /= 255.
+        std /= 255.
 
     # Segmentation model does not upscale the image
     if config.training.architecture == 'encoderdecoder':
         mag = 1
-        x_out, y_out = x, y
-        input_x *= config.training.magnification
-        input_y *= config.training.magnification
+        out = (x, y)
+        tile[0] *= config.training.magnification
+        tile[1] *= config.training.magnification
     else:
         mag = config.training.magnification
-        x_out, y_out = x * mag, y * mag
+        out = (x * mag, y * mag)
+
+    # Check the number of channels
+    if ch == 3 and not config.training.rgb:
+        img_full = np.expand_dims(np.mean(img_full, axis=-1), axis=-1)
+        ch = 1
+    elif ch == 1 and config.training.rgb:
+        img_full = np.repeat(img_full, 2, axis=-1)
+
 
     # Cut large image into overlapping tiles
-    tiler = ImageSlicer(img_full.shape, tile_size=(input_x, input_y),
-                        tile_step=(input_x // tile, input_y // tile), weight=weight)
-
-    x_tile = np.min((input_x * mag, x_out))
-    y_tile = np.min((input_y * mag, y_out))
-    tiler_out = ImageSlicer((x_out, y_out, ch), tile_size=(x_tile, y_tile),
-                            tile_step=(x_tile // tile, y_tile // tile), weight=weight)
+    tiler = Tiler3D(img_full.shape, tile=tile, out=out, step=step, mag=mag, weight=weight)
 
     # HCW -> CHW. Optionally, do normalization here
     tiles = [tensor_from_rgb_image(tile) for tile in tiler.split(img_full)]
 
     # Allocate a CUDA buffer for holding entire mask
-    merger = CudaTileMerger(tiler_out.target_shape, channels=3, weight=tiler_out.weight)
+    merger = TileMerger3D(tiler.target_shape, channels=ch, weight=tiler.weight, cuda=cuda)
 
     # Run predictions for tiles and accumulate them
-    for tiles_batch, coords_batch in DataLoader(list(zip(tiles, tiler_out.crops)), batch_size=config['training']['bs'],
+    for tiles_batch, coords_batch in DataLoader(list(zip(tiles, tiler.crops_out)), batch_size=args.bs,
                                                 pin_memory=True):
         # Move tile to GPU
         if mean is not None and std is not None:
             tiles_batch = tiles_batch.float()
             for ch in range(len(mean)):
-                tiles_batch[:, ch, :, :] = ((tiles_batch[:, ch, :, :] - mean[ch]) / std[ch])
+                tiles_batch[:, ch, :, :] = (((tiles_batch[:, ch, :, :]  / 255.) - mean[ch]) / std[ch])
             tiles_batch = tiles_batch.to(device)
         else:
             tiles_batch = (tiles_batch.float() / 255.).to(device)
@@ -92,16 +98,20 @@ def inference(inference_model, args, config, img_full, device='cuda', weight='me
 
         # Check for inconsistencies
         #if pred_batch.shape[2] > x_out:
-        pred_batch = pred_batch[:, :, :x_tile, :]
+        #pred_batch = pred_batch[:, :, :x_tile, :]
         #if pred_batch.shape[3] > y_out:
-        pred_batch = pred_batch[:, :, :, :y_tile]
+        #pred_batch = pred_batch[:, :, :, :y_tile]
+
+        if not cuda:
+            pred_batch = pred_batch.cpu()
 
         # Merge on GPU
         merger.integrate_batch(pred_batch, coords_batch)
 
     # Normalize accumulated mask and convert back to numpy
     merged_pred = np.moveaxis(to_numpy(merger.merge()), 0, -1).astype('float32')
-    merged_pred = tiler_out.crop_to_orignal_size(merged_pred)
+    merged_pred = tiler.crop_to_orignal_size(merged_pred).squeeze()
+
     # Plot
     if plot:
         for i in range(args.bs):
@@ -114,7 +124,10 @@ def inference(inference_model, args, config, img_full, device='cuda', weight='me
     torch.cuda.empty_cache()
     gc.collect()
 
-    return merged_pred.squeeze()[:, :, 0]
+    if len(merged_pred.shape) == 3:
+        return merged_pred[:, :, 0]
+    else:
+        return merged_pred
 
 
 def inference_3d(inference_model, args, config, img_full, device='cuda', plot=False,
@@ -129,7 +142,7 @@ def inference_3d(inference_model, args, config, img_full, device='cuda', plot=Fa
 
     # Input variables
     mag = config.training.magnification
-    tile = list(config['training']['crop_small'])
+    tile = list(config.training.crop_small)
 
     x, y, z, ch = img_full.shape
     out = (x * mag, y * mag, z * mag)
@@ -156,9 +169,8 @@ def inference_3d(inference_model, args, config, img_full, device='cuda', plot=Fa
     merger = TileMerger3D(tiler.target_shape, channels=ch, weight=tiler.weight, cuda=cuda)
 
     # Run predictions for tiles and accumulate them
-    for tiles_batch, coords_batch in DataLoader(list(zip(tiles, tiler.crops_out)), batch_size=config['training']['bs'],
-                                                #pin_memory=True,
-                                                num_workers=16):
+    for tiles_batch, coords_batch in DataLoader(list(zip(tiles, tiler.crops_out)), batch_size=config.training.bs,
+                                                pin_memory=True):
         # Move tile to GPU
         if mean is not None and std is not None:
             tiles_batch = tiles_batch.float()
@@ -335,7 +347,7 @@ def inference_runner_oof(args, config, split_config, device, plot=False, verbose
                     data = inference_3d(model, args, config, data, step=step, mean=mean, std=std, plot=plot,
                                         weight=weight)
                 else:
-                    data = inference(model, args, config, data, tile=step, mean=mean, std=std, plot=plot,
+                    data = inference(model, args, config, data, step=step, mean=mean, std=std, plot=plot,
                                      weight=weight)
 
             # Scale the dynamic range

bone_enhance/inference/tiler3d.py
@@ -43,20 +43,37 @@ class Tiler3D:
         crops = []
         crops_out = []
         bbox_crops = []
-        for x in range(
-                0, self.input[0] + self.margin_begin[0] + self.margin_end[0] - self.tile[0] + 1, self.step[0]
-        ):
-            for y in range(
-                    0, self.input[1] + self.margin_begin[1] + self.margin_end[1] - self.tile[1] + 1, self.step[1]
-            ):
-                for z in range(
-                        0, self.input[2] + self.margin_begin[2] + self.margin_end[2] - self.tile[2] + 1, self.step[2]
-                ):
-                    crops.append((x, y, z, self.tile[0], self.tile[1], self.tile[2]))
-                    bbox_crops.append((x - self.margin_begin[0], y - self.margin_begin[1], z - self.margin_begin[2],
-                                       self.tile[0], self.tile[1], self.tile[2]))
 
-                    crops_out.append((x * mag, y * mag, z * mag, self.tile_out[0], self.tile_out[1], self.tile_out[2]))
+        # 3D tiling
+        if self.dim == 3:
+            for x in range(
+                    0, self.input[0] + self.margin_begin[0] + self.margin_end[0] - self.tile[0] + 1, self.step[0]
+            ):
+                for y in range(
+                        0, self.input[1] + self.margin_begin[1] + self.margin_end[1] - self.tile[1] + 1, self.step[1]
+                ):
+                    for z in range(
+                            0, self.input[2] + self.margin_begin[2] + self.margin_end[2] - self.tile[2] + 1, self.step[2]
+                    ):
+                        crops.append((x, y, z, self.tile[0], self.tile[1], self.tile[2]))
+                        bbox_crops.append((x - self.margin_begin[0], y - self.margin_begin[1], z - self.margin_begin[2],
+                                           self.tile[0], self.tile[1], self.tile[2]))
+
+                        crops_out.append((x * mag, y * mag, z * mag, self.tile_out[0], self.tile_out[1], self.tile_out[2]))
+        else:
+            for x in range(
+                    0, self.input[0] + self.margin_begin[0] + self.margin_end[0] - self.tile[0] + 1, self.step[0]
+            ):
+                for y in range(
+                        0, self.input[1] + self.margin_begin[1] + self.margin_end[1] - self.tile[1] + 1, self.step[1]
+                ):
+                    crops.append((x, y, self.tile[0], self.tile[1]))
+                    bbox_crops.append((x - self.margin_begin[0], y - self.margin_begin[1],
+                                       self.tile[0], self.tile[1]))
+
+                    crops_out.append(
+                        (x * mag, y * mag, self.tile_out[0], self.tile_out[1]))
+
 
         self.crops = np.array(crops)
         self.crops_out = np.array(crops_out)
@@ -69,20 +86,33 @@ class Tiler3D:
         self.image_pad = size
         image_pad = np.zeros(size + (image.shape[-1],))
         #image_pad[self.margin_begin:image.shape[:-1] + self.margin_begin] = image
-        image_pad[self.margin_begin[0]:image.shape[0] + self.margin_begin[0],
-                  self.margin_begin[1]:image.shape[1] + self.margin_begin[1],
-                  self.margin_begin[2]:image.shape[2] + self.margin_begin[2], :] = image
+        if self.dim == 3:
+            image_pad[self.margin_begin[0]:image.shape[0] + self.margin_begin[0],
+                      self.margin_begin[1]:image.shape[1] + self.margin_begin[1],
+                      self.margin_begin[2]:image.shape[2] + self.margin_begin[2], :] = image
+        else:
+            image_pad[self.margin_begin[0]:image.shape[0] + self.margin_begin[0],
+                      self.margin_begin[1]:image.shape[1] + self.margin_begin[1], :] = image
         image = image_pad
 
         tiles = []
-        for x, y, z, tile_x, tile_y, tile_z in self.crops:
-            tile = image[x: x + tile_x, y: y + tile_y, z: z + tile_z].copy()
-            if tile.shape[0] != self.tile[0]:
-                print(tile.shape[0])
-            assert tile.shape[1] == self.tile[1]
-            assert tile.shape[2] == self.tile[2]
+        if self.dim == 3:
+            for x, y, z, tile_x, tile_y, tile_z in self.crops:
+                tile = image[x: x + tile_x, y: y + tile_y, z: z + tile_z].copy()
+                if tile.shape[0] != self.tile[0]:
+                    print(tile.shape[0])
+                assert tile.shape[1] == self.tile[1]
+                assert tile.shape[2] == self.tile[2]
 
-            tiles.append(tile)
+                tiles.append(tile)
+        else:
+            for x, y, tile_x, tile_y in self.crops:
+                tile = image[x: x + tile_x, y: y + tile_y].copy()
+                if tile.shape[0] != self.tile[0]:
+                    print(tile.shape[0])
+                assert tile.shape[1] == self.tile[1]
+
+                tiles.append(tile)
         return tiles
 
     def crop_to_orignal_size(self, image):
@@ -97,11 +127,17 @@ class Tiler3D:
         ]
         """
         n = 5
-        crop = image[
-               n:self.out[0] + n,
-               n:self.out[1] + n,
-               n:self.out[2] + n,
-               ]
+        if self.dim == 3:
+            crop = image[
+                   n:self.out[0] + n,
+                   n:self.out[1] + n,
+                   n:self.out[2] + n,
+                   ]
+        else:
+            crop = image[
+                   n:self.out[0] + n,
+                   n:self.out[1] + n,
+                   ]
         return crop
 
     def merge(self, tiles: List[np.ndarray], channels, dtype=np.float16):
@@ -139,7 +175,10 @@ class Tiler3D:
     def _gaussian(self, tile_size, step):
         m = np.zeros(tile_size, dtype=np.float32)
         dim = np.array(m.shape) // 2
-        m[dim[0], dim[1], dim[2]] = 1
+        if len(dim) == 3:
+            m[dim[0], dim[1], dim[2]] = 1
+        else:
+            m[dim[0], dim[1]] = 1
         return gaussian_filter(m, sigma=dim[0] // step)
 
 
@@ -157,8 +196,12 @@ class TileMerger3D:
         """
 
         self.weight = torch.from_numpy(np.expand_dims(weight, axis=0)).float()
-        self.image = torch.zeros((channels, image_shape[0], image_shape[1], image_shape[2]), dtype=torch.float32)
-        self.norm_mask = torch.zeros((1, image_shape[0], image_shape[1], image_shape[2]), dtype=torch.float32)
+        if len(image_shape) == 3:
+            self.image = torch.zeros((channels, image_shape[0], image_shape[1], image_shape[2]), dtype=torch.float32)
+            self.norm_mask = torch.zeros((1, image_shape[0], image_shape[1], image_shape[2]), dtype=torch.float32)
+        else:
+            self.image = torch.zeros((channels, image_shape[0], image_shape[1]), dtype=torch.float32)
+            self.norm_mask = torch.zeros((1, image_shape[0], image_shape[1]), dtype=torch.float32)
         if cuda:
             self.weight = self.weight.cuda()
             self.image = self.image.cuda()
@@ -173,9 +216,16 @@ class TileMerger3D:
         if len(batch) != len(crop_coords):
             raise ValueError("Number of images in batch does not correspond to number of coordinates")
 
-        for tile, (x, y, z, tile_x, tile_y, tile_z) in zip(batch, crop_coords):
-            self.image[:, x: x + tile_x, y: y + tile_y, z: z + tile_z] += tile * self.weight
-            self.norm_mask[:, x: x + tile_x, y: y + tile_y, z: z + tile_z] += self.weight
+        # 3D merge
+        if len(batch.size()) == 5:
+            for tile, (x, y, z, tile_x, tile_y, tile_z) in zip(batch, crop_coords):
+                self.image[:, x: x + tile_x, y: y + tile_y, z: z + tile_z] += tile * self.weight
+                self.norm_mask[:, x: x + tile_x, y: y + tile_y, z: z + tile_z] += self.weight
+        # 2D merge
+        else:
+            for tile, (x, y, tile_x, tile_y) in zip(batch, crop_coords):
+                self.image[:, x: x + tile_x, y: y + tile_y] += tile * self.weight
+                self.norm_mask[:, x: x + tile_x, y: y + tile_y] += self.weight
 
     def merge(self) -> torch.Tensor:
         return self.image / self.norm_mask

scripts/inference_tiles_2d.py
@@ -127,7 +127,7 @@ if __name__ == "__main__":
         with torch.no_grad():  # Do not update gradients
 
             for slice_idx in tqdm(range(data_xy.shape[2]), desc='Running inference, XY'):
-                out_xy[:, :, slice_idx] = inference(model, args, config, data_xy[:, :, slice_idx, :], tile=args.tile,
+                out_xy[:, :, slice_idx] = inference(model, args, config, data_xy[:, :, slice_idx, :], step=args.tile,
                                                     mean=mean, std=std)
 
         # Scale the dynamic range

scripts/inference_tiles_3d.py
@@ -166,7 +166,7 @@ if __name__ == "__main__":
                     out = np.zeros((data.shape[0], data.shape[1], data.shape[2]))
                     for slice_idx in tqdm(range(data.shape[2]), desc='Running inference, XY'):
                         out[:, :, slice_idx] = inference(model, args, config, data[:, :, slice_idx, :],
-                                                         tile=args.step, weight=args.weight,
+                                                         step=args.step, weight=args.weight,
                                                          mean=mean, std=std)
                     data = out
                 else: